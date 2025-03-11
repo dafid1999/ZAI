a4d245257ddf20e25a6e48fb501e6619431c2471
Dawid Borawski <58917193+dafid1999@users.noreply.github.com>
2025-03-11 14:41:07 +0100

    Part 2 Django basic 2 completed

--- filmy/urls.py
+++ filmy/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from filmy.views import wszystkie, szczegoly
 
 urlpatterns = [
-    path('wszystkie/', wszystkie),
-    path('szczegoly/<int:film_id>/', szczegoly)
+    path('wszystkie/', wszystkie, name='wszystkie'),
+    path('szczegoly/<int:film_id>/', szczegoly, name='szczegoly'),
 ]
--- filmy/views.py
+++ filmy/views.py
@@ -1,14 +1,17 @@
 from django.http import HttpResponse
-
+from django.template import loader
 from filmy.models import Film
 
 
 def wszystkie(request):
-    return HttpResponse(["<h1>", [[f.id, f.tytul, f.rok] for f in Film.objects.all()], "</h1>"])
+    template = loader.get_template("filmy/wszystkie.html")
+    wszystkie_filmy = Film.objects.all()
+    context = {'wszystkie_filmy':wszystkie_filmy,}
+    return HttpResponse(template.render(context, request))
 
+def szczegoly(request,film_id):
+    template = loader.get_template("filmy/szczegoly.html")
+    film = Film.objects.get(id=film_id)
+    context = {'film': film}
+    return HttpResponse(template.render(context,request))
 
-def szczegoly(request, film_id):
-    f = Film.objects.get(id=film_id)
-    return HttpResponse(
-        "<h3> Tytuł filmu: {},</br> rok produkcji: {}, </br> data premiery: {}, </br> opis: {}, </br> punkty od widzów: {} </h3>"
-        .format(f.tytul, f.rok, f.premiera, f.opis, f.imdb_pkts))
