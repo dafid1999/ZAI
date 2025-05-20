0e2da3060c992e64bbfad37bbada554c176649cb
Dawid Borawski <58917193+dafid1999@users.noreply.github.com>
2025-05-20 17:21:24 +0200

    Register models in admin.py

--- ZAI_Projekt/listings/admin.py
+++ ZAI_Projekt/listings/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Profile, Category, Tag, Listing
 
-# Register your models here.
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'phone_number')
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'status', 'created_at')
+    list_filter = ('status', 'category', 'created_at')
+    search_fields = ('title', 'description', 'author__username')
+    raw_id_fields = ('author',)
+    filter_horizontal = ('tags', 'favorited_by')
